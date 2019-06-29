4d86d7d511a5af2fa1b13288bbaf939f614f6e74
NamikawaRyotaro <n.ryo0328@gmail.com>
2019-06-29 19:20:29 +0900

    fix output github

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,7 +1,11 @@
 class Fizzbuzz
   def calculate number
     if number % 3 == 0 && number % 5 == 0
-      'fizzbuzz'
+      if number.to_s.include? '7'
+        'GitHub'
+      else
+        'fizzbuzz'
+      end
     elsif number % 3 == 0
       'fizz'
     elsif number % 5 == 0
